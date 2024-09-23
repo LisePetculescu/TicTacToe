b1703a8e8aa1998ee5472da9b639ad1a53c16938
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-09-23 13:42:17 +0200

    gg

---
+++ view.js
@@ -0,0 +1,19 @@
+export { init };
+
+function init() {
+  console.log("view kører");
+
+  const alleCeller = document.querySelectorAll(".cell");
+  alleCeller.forEach((cell) => cell.addEventListener("click", clickCell));
+}
+
+function clickCell(event) {
+  console.log("cellen er klikket");
+
+  console.log("klik på: ", event.target.dataset);
+  const row = event.target.dataset["row"];
+  const col = event.target.dataset["col"];
+  //   console.log("x-cord: ", row);
+  //   console.log("y-cord: ", col);
+  console.log(`clicked on row: ${row} & col: ${col}`);
+}
